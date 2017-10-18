ce3bbaedf0005cfc99a9e2d6d51b63957bcc4bfd
jokerbmbm97 <jokerbmbm97gmail.com>
2017-10-18 16:47:20 +0100

    add goodbye print

--- lol.py
+++ lol.py
@@ -1,36 +1,3 @@
-__author__ = 'Xazlin'
+("hello world")
 
-
-def test():
-    array = [[3872, -912],[3872, -912],[3874, -922],[3874, -922],[3874, -912],[3883, -909],[3883, -909],[3883, -909]]
-
-    print findgroups(array)
-
-
-def findgroups(array):
-    seen = []
-    seen2 = []
-    index = []
-    result = []
-
-    for i in array:
-        if i[1] not in seen:
-            seen.append(i[1])
-            seen2.append(i)
-
-    for i in seen2:
-        index.append(array.index(i))
-
-    print index
-
-    for i in range(len(index)):
-        current = index[i]
-        last = len(array)
-        if i != len(index)-1:
-            last = index[i+1]-1
-        if current != last:
-            result.append([current, last])
-
-    return result
-
-test()
+("Goodbye")
